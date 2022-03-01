eda6d10bcdb02e94e57717ae9ebdf606e2741d6c
segundo rebaza <alvaro.rp1593@gmail.com>
2022-02-28 23:46:13 -0500

    se agrego seed con api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
 
-Movie.destroy_all
-
-10.times do
-  movie = Movie.new(
-    overview: Faker::Movie.quote,
-    title: Faker::Movie.title,
-    poster_url: Faker::LoremFlickr.image(size: "50x60"),
-    rating: Faker::Number.within(range: 1..10)
-  )
-  movie.save!
+list_movies = URI.open('http://tmdb.lewagon.com/movie/top_rated').read
+movies = JSON.parse(list_movies)
+movies["results"].each do |movie|
+  if movie["original_language"] == 'en'
+    new_movie = Movie.new
+    new_movie["title"] = movie["original_title"]
+    new_movie["overview"] = movie["overview"]
+    new_movie["poster_url"] = "https://image.tmdb.org/t/p/w500/#{movie["poster_path"]}"
+    new_movie["rating"] = movie["vote_average"]
+    new_movie.save
+  end
 end
-puts 'Finished!'
+
+puts 'Seed finalizado'
+
+# 10.times do
+#   movie = Movie.new(
+#     overview: Faker::Movie.quote,
+#     title: Faker::Movie.title,
+#     poster_url: Faker::LoremFlickr.image(size: "50x60"),
+#     rating: Faker::Number.within(range: 1..10)
+#   )
+#   movie.save!
+# end
+# puts 'Finished!'
