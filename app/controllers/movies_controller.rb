e484ef7d82959189d1df5d9942ef7ec7ad8e0259
segundo rebaza <alvaro.rp1593@gmail.com>
2022-04-04 00:45:44 -0500

    add basic search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,9 @@
 class MoviesController < ApplicationController
   def index
     @movies = Movie.all
+
+    if params[:query].present?
+      @movies = @movies.where('title iLIKE ?', "%#{params[:query]}%")
+    end
   end
 end
